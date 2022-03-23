5ec892a500c65828ee50f6ca2c9402904177f17d
Aleksander <aleksandros-777@mail.ru>
2022-03-23 10:13:16 +0300

    added work with react app

--- deno/app.ts
+++ deno/app.ts
@@ -4,12 +4,25 @@ import todoRoutes from "./routes/todos.ts";
 
 const app = new Application();
 
+// To use middleware we should make function async
 app.use(async (ctx, next) => {
   console.log("Middleware");
   await next();
 });
 
+// Add headers to fix CORS error in SPA front-end application
+app.use(async (ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE"
+  );
+  ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  await next();
+});
+
+// For routes to work
 app.use(todoRoutes.routes());
 app.use(todoRoutes.allowedMethods());
 
-await app.listen({ port: 3000 });
+await app.listen({ port: 8000 });
